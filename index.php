f920a63a20a0fbc935696c8c1fd64af2465080ed
johnny1114 <johnny1114@users.noreply.github.com>
2026-03-02 23:14:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+// Option A: Using DateTime (best practice - handles timezones cleanly)
+$date = new DateTime('now', new DateTimeZone('America/New_York'));
+echo $date1 = $date->format('Y-m-d H:i:s');          // 2026-03-02 23:15:42
+echo $date->format('F j, Y g:i:s A');       // March 2, 2026 11:15:42 PM
+echo $date->format('l, F j, Y \a\t g:i A'); // Sunday, March 2, 2026 at 11:15 PM
 
-echo "Hello World!";
+echo "HighWing Innovations @ ".$date1;
